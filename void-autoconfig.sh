ccb2d1b874dcaf81fb491415662e6a6c2b125221
mkonji <brideoninoue@gmail.com>
2023-01-06 00:16:44 -0500

    work in progress, added several files and updated primary script

--- void-autoconfig.sh
+++ void-autoconfig.sh
@@ -6,37 +6,93 @@ echo "Void Linux autoconfig By-mkonji "
 exec > >(tee logfile.txt)
 
 ##
-# Update the xbps package manager and update Void
-sudo xbps-install -Syu xbps
-sudo xbps-install -Suv
+# Update the xbps package manager 
+if sudo xbps-install -Syu xbps; then
+  echo "Installed/Updated xbps."
+else
+  echo "Error installing/updating xbps. Exiting."
+  exit 1
+fi
+
+##
+# Update Void Linux
+if sudo xbps-install -Suv; then
+  echo "Updating Void Linux."
+else
+  echo "Error updating Void Linux. Exiting."
+  exit 1
+fi
+
+##
+# Restart services post system update
+if sudo xcheckrestart; then
+  echo "Restarting updated services."
+else
+  echo "Error restarting services. Exiting."
+  exit 1
+
+##
+# Enable nonfree repositories
+sudo xbps-install void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
+echo "Enable non-free repositories."
+sudo xbps-install void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree && \
+echo "Developer packages installed." || \
+echo "Error enabling non-free repos. Exiting." && exit 1
 
 ##
 # Install essential packages
-sudo xbps-install -S vim curl wget xz unzip zip vim gptfdisk xtools mtools mlocate ntfs-3g fuse-exfat bash-completion linux-headers gtksourceview4 ffmpeg mesa-vdpau mesa-vaapi htop neofetch timeshift ranger
+echo "Installing essential packages."
+sudo xbps-install -S vim curl wget xz unzip zip vim gptfdisk xtools mtools mlocate ntfs-3g fuse-exfat bash-completion linux-headers gtksourceview4 ffmpeg mesa-vdpau mesa-vaapi htop neofetch timeshift ranger && \
+echo "Developer packages installed." || \
+echo "Error installing essential packages. Exiting." && exit 1
 
 ##
 # Install developer Packages
-sudo xbps-install autoconf automake bison m4 make libtool flex meson ninja optipng sassc python python3 python3-pip 
+echo "Installing developer packages."
+sudo xbps-install -S autoconf automake bison m4 make libtool flex meson ninja optipng sassc python python3 python3-piph&& \
+echo "Developer packages installed." || \
+echo "Error installing developer packages. Exiting." && exit 1
 
 ##
 # Install security
-sudo xbps-install -S gpg gpg2 yadm pass
+echo "Installing security packages."
+sudo xbps-install -S gpg gpg2 yadm pass && \
+echo "Installation of security packages complete." || \
+echo "Error installing security packages. Exiting." && exit 1
 
 ##
 # Install xorg, dbus and elogind and enable their services
-sudo xbps-install -S xorg dbus elogind
-sudo ln -s /etc/sv/dbus /var/service
-sudo ln -s /etc/sv/elogind /var/service
+echo "Installing xorg, dbus, and elogind and enabling services."
+sudo xbps-install -S xorg dbus elogind && \
+sudo ln -s /etc/sv/dbus /var/service/ && \
+sudo ln -s /etc/sv/elogind /var/service/ && \
+echo "Installation and configuration completed" || \
+echo "Error. Exiting." && exit 1
 
+##
+# Install SpacevVim
+echo "Instaling SpaceVim."
+
+curl -sLf https://spacevim.org/install.sh | bash
+cp SpaceVim/init.toml ~/.SpaceVim.d/init.toml
+
+echo "SpaceVim Installed. Please run vim post script completion to auto install plugins"
+
+## Install WM/DE
+#
 echo "Enter 'i3', 'bspwm', 'gnome', or 'kde' to install the corresponding window manager: "
 read wm
+echo "Now Installing and Configuring $wm."
 
 # Install selected window manager and copy configuration file
 if [ "$wm" == "i3" ]; then
   sudo xbps-install -S i3
-  cp i3/config ~/.config/i3/confignerd-fonts
+  mkdir ~/.config/i3
+  cp i3/config ~/.config/i3/config
+  mkdir ~/.config/polybar
+  cp polybar/config.ini ~/.config/polybar/config.ini
 elif [ "$wm" == "bspwm" ]; then
-  sudo xbps-install -S bspwm
+  sudo xbps-install -S bspwm sxhkd polybar
   cp bspwm/config ~/.config/bspwm/config
 elif [ "$wm" == "gnome" ]; then
   sudo xbps-install -s gnome gdm
@@ -59,29 +115,11 @@ fi
 
 echo "WM/DE Installation complete, moving onto next step."
 
-#######
-# Install a desktop enviornment 
-echo "Enter 'i3' to install i3 window manager, 'bspwm' to install bspwm, 'xfce' to install 'xfce', or 'kde' to install KDE: "
-read input
-
-if [ "$input" == "i3" ]; then
-  sudo xbps-install -S i3 i3lock polybar nm-applet xfce4-power-manager picom nitrogen xrandr arandr flameshot copyq pulseaudio pavucontrol thunar rofi blueman
-  cp i3/myconfigfile.txt ~/.config/i3/
-elif [ "$input" == "bspwm" ]; then
-  sudo xbps-install -S bspwm sxhkd polybar  
-elif [ "$input" == "kde" ]; then
-  sudo xbps-install -S kde 
-else
-  echo "Invalid input. Exiting."
-  exit 1
-fi
-
-# Install SpacevVim
-curl -sLf https://spacevim.org/install.sh | bash
-
-  cp SpaceVim/init.toml ~/.SpaceVim.d/init.toml
-echo "Installation complete."
-
+##
+# Pull void-src and configure it
+git clone https://github.com/void-linux/void-packages.git
+cd void-packages
+./xbps-src binary-bootstrap
 
 ## 
 # Display Configuration
@@ -138,8 +176,10 @@ echo "NoTear option added to Xorg configuration file."
 # Install Cronie for cron jobs
 echo "Installing and Configuring Cronie."
 
-sudo xbps-install -y cronie
-sudo ln -sv /etc/sv/cronie /var/service
+sudo xbps-install -y cronie && \
+sudo ln -sv /etc/sv/cronie /var/service/ && \
+echo "Installation of Cronie completed." || \
+echo "Error installing and enabling Cronie service. Exiting." && exit 1
 
 ##
 # Install TLP and PowerTop if this is a laptop
@@ -158,29 +198,32 @@ fi
 # Logging Daemon Activation
 # Install a logging Daemon as void does not have one by default
 echo "Logging Daemon Activation."
-
-sudo xbps-install -Rs socklog-void
-sudo ln -s /etc/sv/socklog-unix /var/service/
-sudo ln -s /etc/sv/nanoklogd /var/service/
+sudo xbps-install -Rs socklog-void && \
+sudo ln -s /etc/sv/socklog-unix /var/service/ && \
+sudo ln -s /etc/sv/nanoklogd /var/service/ && \
+echo "Successfully installed and configured Cronie." || \
+echo "Error installing and enabling Cronie service. Exiting." && exit 1
 
 ##
 # Configure Profile Sync Daemon 
 # set up profile sync daemon PSD is a service that symlinks & syncs browser profile
 # directories to RAM, thus reducing HDD/SSD calls & speeding up browsers. You can get
 # it from here. This helps Firefox & Chromium reduce ram usage.
-echo "Configure Profile Sync Daemon."
-
-git clone https://github.com/madand/runit-services
-cd runit-services
-sudo mv psd /etc/sv/
-sudo ln -s /etc/sv/psd /var/service/
-sudo chmod +x etc/sv/psd/*
+echo "Pull and Configure Profile Sync Daemon."
+git clone https://github.com/madand/runit-services && \
+cd runit-services && \
+sudo mv psd /etc/sv/ && \
+sudo ln -s /etc/sv/psd /var/service/ && \
+sudo chmod +x etc/sv/psd/* && \
+echo "Successfully installed and configured Profile Sync Daemon." || \
+echo "Failure installing and configuring Profile Sync Daemon. Exiting." && exit 1
 
 ##
 # Install fonts 
 echo "Installing Fonts."
-
-sudo xbps-install -Rs noto-fonts-emoji noto-fonts-ttf noto-fonts-ttf-extra nerd-fonts
+sudo xbps-install -Rs noto-fonts-emoji noto-fonts-ttf noto-fonts-ttf-extra nerd-fonts && \
+echo "Installed Noto and Nerd Fonts." || \
+echo "Error installing Noto and Nerd Fonts. Exiting." && exit 1
 
 # Install Microsoft fonts for compatibility
 git clone https://github.com/void-linux/void-packages
@@ -190,8 +233,23 @@ echo "XBPS_ALLOW_RESTRICTED=yes" >> etc/conf
 
 ##
 # Install Syncthing and enable autostart
-echo "Installing Syncthing."
+echo "Installing Syncthing." 
+sudo xbps-install -Rs syncthing && \
+sudo cp /usr/share/applications/syncthing-start.desktop ~/.config/autostart/ && \
+echo "Installed Syncthing. Please configure manually." || \
+echo "Failure to install Syncthing. Exiting." && exit 1
 
-sudo xbps-install -Rs syncthing
-sudo cp /usr/share/applications/syncthing-start.desktop ~/.config/autostart/
-
+##
+# Configure Fish Shell
+cd ~/git/void-autoconfig
+mkdir ~/.config/fish/
+touch ~/.config/fish/config.fish
+# Read alias definitions from a file and add them to the Fish shell config
+while read -r alias; do
+  if echo "$alias" >> ~/.config/fish/config.fish; then
+    echo "Alias added: $alias"
+  else
+    echo "Error adding alias: $alias"
+  fi
+done < fish-aliases.txt
+echo "Aliases added."
